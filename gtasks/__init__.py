93c271a8159089cc17470f037896cd847acaeddd
Ben White <ben.white@berkeley.edu>
2014-11-14 14:06:18 -0800

    Workaround for oauthlib 0.7 bug involving scope checking

--- gtasks/__init__.py
+++ gtasks/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import
 
-__version__='0.1.2'
+import os
+
+__version__='0.1.3'
+
+# Requred for a application-breaking bug in requests-oauthlib/oauthlib
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = 'workaround'
 
 from gtasks.gtasks import Gtasks
